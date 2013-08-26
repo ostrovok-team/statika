b12609c4a7eb48dcc9c5f5d68981cb9414a5838e
Egor <egor.orlov@ostrovok.ru>
2013-08-26 19:18:34 +0400

    replaced watchdog with pyinotify

--- src/statika/dev.py
+++ src/statika/dev.py
@@ -9,7 +9,7 @@ from watchdog.observers import Observer
 from statika import build
 
 def start_watcher(watched_dir, bundles, build_func=None, logger=None,
-                  file_patterns=['*.js', '*.css']):
+                  file_patterns=['*.css', '*.js', '*.html']):
     if build_func is None:
         build_func = build
     if logger is None:
@@ -20,7 +20,7 @@ def start_watcher(watched_dir, bundles, build_func=None, logger=None,
         if not event.src_path or filename[0] in ('.', '_'):
             return
         ext = path.splitext(filename)[1]
-        logger.debug(' - %s is modified, rebuild static' % event.src_path)
+        logger.debug(' - %s is modified, rebuild static', event.src_path)
         build_func(bundles)
 
     def builder_process():
@@ -38,54 +38,6 @@ def start_watcher(watched_dir, bundles, build_func=None, logger=None,
                 time.sleep(1)
         except KeyboardInterrupt:
             observer.stop()
-
-    build_func(bundles)
-    _watcher = Process(target=builder_process, name='killa')
-    _watcher.start()
-
-
-if __name__ == '__main__':
-    if len(argv) > 1 and path.isdir(argv[1]):
-        watched_dir = argv[1]
-    else:
-        watched_dir = getcwd()
-    start_watcher(watched_dir, argv[2:])
-from logging import getLogger
-from multiprocessing import Process
-from os import path, getcwd
-from sys import argv
-
-from pyinotify import WatchManager, Notifier, IN_MODIFY, IN_CREATE, IN_DELETE
-
-from statika import build
-
-
-def start_watcher(watched_dir, bundles, build_func=None, logger=None,
-                  file_types=('js', 'css', 'html')):
-    if build_func is None:
-        build_func = build
-    if logger is None:
-        logger = getLogger(__name__)
-
-    def _build(event):
-        if not event.name or event.name[0] in ('.', '_'):
-            return
-        ext = path.splitext(event.name)[1]
-        if ext not in file_types:
-            return
-        logger.debug(' - %s is modified, rebuild static' % event.pathname)
-        build_func(bundles)
-
-    def builder_process():
-        logger.debug(' - Watched static files for changes to rebuild')
-        wm = WatchManager()
-        notifier = Notifier(wm, default_proc_fun=_build)
-        wm.add_watch(
-            watched_dir,
-            IN_MODIFY, # | IN_CREATE | IN_DELETE,
-            rec=True, auto_add=True
-        )
-        notifier.loop()
 
     build_func(bundles)
     _watcher = Process(target=builder_process, name='killa')
